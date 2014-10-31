df077534b901e8f88676d35625224c279007def8
Tae <taehyun0313@gmail.com>
2014-10-31 16:30:51 +0900

    hdftest.R

--- hdftest.R
+++ hdftest.R
@@ -1,7 +1,10 @@
 library(rhdf5)
 #h5createFile("basefile.h5")
 
-A <- read.csv("randomintegers.csv")
+#A <- read.csv("randomintegers.csv")
 
 #h5createGroup("basefile.h5", "group1")
 #h5write(A, "basefile.h5", "group1/A")
+
+h5ls("basefile.h5")
+
